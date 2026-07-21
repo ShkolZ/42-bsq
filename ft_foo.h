d4a23345e9ea19889cc43414b227ecee126b3277
ShkolZ <victorbertuch@gmail.com>
2026-07-22 00:51:33 +0200

    should work on some level

--- ft_foo.h
+++ ft_foo.h
@@ -6,7 +6,7 @@
 /*   By: vbertych <vbertych@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/18 19:15:55 by reyam             #+#    #+#             */
-/*   Updated: 2026/07/21 03:18:15 by vbertych         ###   ########.fr       */
+/*   Updated: 2026/07/22 00:49:56 by vbertych         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,17 +41,21 @@ typedef struct s_point
 
 typedef struct s_square
 {
-	t_point	start;
+	t_point	top_left;
+	t_point	top_right;
+	t_point	bot_left;
+	t_point	bot_right;
 	int		side;
-	char	**arr;
 }	t_square;
 
-void	*gc_malloc(t_gc **gc, size_t size);
-void	gc_free_all(t_gc **gc);
-int		check_stdin(int argc, char **argv);
-void	free_square(t_square *sq);
-int		ft_abs(int n);
-
+void		*gc_malloc(t_gc **gc, size_t size);
+void		gc_free_all(t_gc **gc);
+int			check_stdin(int argc, char **argv);
+int			ft_abs(int n);
+t_point		make_point(int row, int col);
+int			in_boundaries(t_point point, t_map *map);
+int			is_obstacle(t_point point, t_map *map);
+t_square	find_largest(t_map *map);
 
 
 #endif
--- helpers.c
+++ helpers.c
@@ -1,18 +1,44 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   math.c                                             :+:      :+:    :+:   */
+/*   helpers.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vbertych <vbertych@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/21 03:17:04 by vbertych          #+#    #+#             */
-/*   Updated: 2026/07/21 03:17:43 by vbertych         ###   ########.fr       */
+/*   Updated: 2026/07/22 00:38:19 by vbertych         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "ft_foo.h"
 
 int	ft_abs(int n)
 {
 	if (n < 0)
 		return (-n);
 	return (n);
+}
+
+t_point	make_point(int row, int col)
+{
+	t_point	xy;
+
+	xy.row = row;
+	xy.col = col;
+	return (xy);
+}
+
+int	in_boundaries(t_point point, t_map *map)
+{
+	if ((point.row >= 0 && point.row < map->height)
+		&& (point.col >= 0 && point.col < map->len))
+		return (1);
+	return (0);
+}
+
+int	is_obstacle(t_point point, t_map *map)
+{
+	if (map->arr[point.row][point.col] == map->obstacle)
+		return (1);
+	return (0);
 }
