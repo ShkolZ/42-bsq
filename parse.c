50094794b85e33630bfb11ef6e55153a27e6cb02
rimayer <reyam6252@gmail.com>
2026-07-22 22:48:33 +0200

    parse split into two files for norminette.

--- parse.c
+++ parse.c
@@ -3,51 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   parse.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vbertych <vbertych@student.42prague.com    +#+  +:+       +#+        */
+/*   By: reyam <reyam@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/18 19:15:55 by reyam             #+#    #+#             */
-/*   Updated: 2026/07/22 22:07:22 by vbertych         ###   ########.fr       */
+/*   Updated: 2026/07/22 22:46:58 by reyam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_foo.h"
-#include <fcntl.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <limits.h>
 
-int	ft_strlen(char *str)
-{
-	int	len;
-
-	len = 0;
-	while (str[len])
-		len++;
-	return (len);
-}
-
-int	ft_atoi(char *str)
-{
-	int	i;
-	int	nbr;
-
-	i = 0;
-	nbr = 0;
-	while (str[i] >= '0' && str[i] <= '9')
-	{
-		nbr = nbr * 10 + (str[i] - '0');
-		i++;
-	}
-	return (nbr);
-}
-
+//malloc line for int_max, read chars one by one byte into char *line.
 char	*get_next_line(int fd)
 {
 	char	*line;
 	char	c;
 	int		len;
 	ssize_t	bytes;
-	//malloc line for int_max, read chars one by one byte into char *line.
+	
 	line = malloc(sizeof(char) * INT_MAX);
 	if (!line)
 		return (NULL);
@@ -76,31 +48,7 @@ char	*get_next_line(int fd)
 	return (line);
 }
 
-int	row_len(char *line)
-{
-	int	len;
 
-	len = 0;
-	while (line[len] && line[len] != '\n')
-		len++;
-	return (len);
-}
-
-int	valid_row(char *line, int len, t_map *map)
-{
-	int	i;
-
-	if (line[len] != '\n')
-		return (0);
-	i = 0;
-	while (i < len)
-	{
-		if (line[i] != map->empty && line[i] != map->obstacle)
-			return (0);
-		i++;
-	}
-	return (1);
-}
 
 char	*copy_row(char *line, int len, t_gc **gc)
 {
